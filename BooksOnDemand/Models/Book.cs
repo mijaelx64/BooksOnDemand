2cd4e220ee8b26a8496fbfc61a2bf953cdccdd15
Mijael Vargas <mijael.vargas007@gmail.com>
2017-02-27 12:41:19 -0400

    Adding Entities

---
+++ BooksOnDemand/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BooksOnDemand.Models
+{
+    public class Book
+    {
+        public ObjectId Id { get; set; }
+        public List<string> Authors { get; set; }
+        public string Description { get; set; }
+        public string Publisher { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ BooksOnDemand/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BooksOnDemand.Models
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+    }
+}
